16c87e710c17c8279dd229fa2977b349fd0d193d
Wagner Coche <wagnercoche@gmail.com>
2024-02-16 01:31:23 +0100

    first commit

---
+++ src/main/java/com/pulsecheckapi/PulseCheckApiApplication.java
@@ -0,0 +1,13 @@
+package com.pulsecheckapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseCheckApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulseCheckApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pulsecheckapi/PulseCheckApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulsecheckapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseCheckApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
